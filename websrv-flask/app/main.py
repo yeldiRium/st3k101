da317291930cbef19db46fb69f027628a03b123b
Noah Hummel <strangedev@posteo.net>
2017-11-09 14:56:17 +0100

    ADD flask app volume to docker-compose

--- websrv-flask/app/main.py
+++ websrv-flask/app/main.py
@@ -5,16 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-
-    memc = Client("memcached", debug=1)
-
-    if not memc.get("test"):
-        return "Visit again"
-
-    memc.set("test", True)  # FIXME
-
-    return "memcached working"
-
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    return "Bring caffeine."
