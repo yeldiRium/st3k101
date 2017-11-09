d8cc189c192e03578643490b5609ded2bc03e11e
Hannes Leutloff <hannes.leutloff@aol.de>
2017-11-09 19:08:39 +0100

    add empty routes according to current frontend plans

--- websrv-flask/app/main.py
+++ websrv-flask/app/main.py
@@ -4,6 +4,76 @@ from memcache import Client
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
+    """
+    Home route
+    """
     return "Bring caffeine, du ficker."
+
+
+# SESSION STUFF:
+@app.route("/register", methods=["GET"])
+def register():
+    """
+    Registration Route
+    """
+    return ""
+
+
+@app.route("/login", methods=["GET"])
+def login():
+    """
+    Login Route
+    """
+    return ""
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    """
+    Logout Route
+    """
+    return ""
+
+
+# USER BACKEND
+@app.route("be/dash", methods=["GET"])
+def user_dashboard():
+    """
+    Dashboard for users
+    """
+    return ""
+
+
+@app.route("be/surveys", methods=["GET"])
+def user_survey_overview():
+    """
+    Survey overview for users
+    """
+    return ""
+
+
+@app.route("be/surveys/<string:survey_uuid>", methods=["GET"])
+def user_survey_details(survey_uuid):
+    """
+    Survey details for users
+    """
+    return ""
+
+
+@app.route("be/account", methods=["GET"])
+def user_account():
+    """
+    Account management for users
+    """
+    return ""
+
+
+# SURVEY FOR DATA SUBJECT
+@app.route("survey/<string:survey_uuid", methods=["GET"])
+def survey(survey_uuid):
+    """
+    Survey
+    """
+    return ""
