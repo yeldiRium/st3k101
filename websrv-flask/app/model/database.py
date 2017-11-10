d52b683ca401eba5b24cd45e96070833f4e13b2a
Noah Hummel <strangedev@posteo.net>
2017-11-10 19:43:28 +0100

    ADD database: DefaultEncoder, DefaultDecoder

--- websrv-flask/app/model/database.py
+++ websrv-flask/app/model/database.py
@@ -1,17 +1,45 @@
 from flask import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from pydoc import safeimport
+
 
 _client = MongoClient("db-mongo")
+_db = _client['efla-web']
+
+
+def classname(o):
+    """
+    Resolves to model.modulename.classname
+    :param o: object
+    :return: str
+    """
+    return str(o.__class__)[8:-2]
+
+
+class DefaultEncoder(object):
+
+    @staticmethod
+    def encode(o):
+        fields = {k: v for k, v in o.__dict__.items() if not k.startswith("_")}
+        return {'classname': classname(o), 'fields': fields}
+
+
+class DefaultDecoder(object):
+
+    @staticmethod
+    def decode(d):
+        if not ('classname' in d and 'fields' in d):
+            raise TypeError
+        return d['fields']
 
 
 class PersistentObject(object):
 
-    __client = _client
-
+    __db = _db
     __collection = None
-    __encoder = None  # type: json.JSONEncoder
-    __decoder = None  # type: json.JSONDecoder
+    _encoder = DefaultEncoder
+    _decoder = DefaultDecoder
 
     def __init__(self, uuid: str):
         """
