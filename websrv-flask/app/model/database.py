9687b9858a889e98714ff1a2e4c64d22faebd6c7
Noah Hummel <strangedev@posteo.net>
2017-11-10 20:00:48 +0100

    UPDATE database.DefaultEncoder: allow private fields to be saved; FIX database.PersistentObject: save bson.ObjectId instead of str

--- websrv-flask/app/model/database.py
+++ websrv-flask/app/model/database.py
@@ -21,7 +21,7 @@ class DefaultEncoder(object):
 
     @staticmethod
     def encode(o):
-        fields = {k: v for k, v in o.__dict__.items() if not k.startswith("_")}
+        fields = {k: v for k, v in o.__dict__.items() if not 'internal_' in k}
         return {'classname': classname(o), 'fields': fields}
 
 
@@ -36,30 +36,30 @@ class DefaultDecoder(object):
 
 class PersistentObject(object):
 
-    __db = _db
-    __collection = None
-    _encoder = DefaultEncoder
-    _decoder = DefaultDecoder
+    __internal_db = _db
+    __internal_collection = None
+    _internal_encoder = DefaultEncoder
+    _internal_decoder = DefaultDecoder
 
     def __init__(self, uuid: str=None):
         """
         TODO: lock object for duration of request
         :param query: 
         """
-        self._id = None
-        self.__collection = self.__db[classname(self)]
+        self._internal_id = None
+        self.__internal_collection = self.__internal_db[classname(self)]
         if uuid:
-            self.__from_document(self.__collection.find_one({u'_id': ObjectId(uuid)}))
-            self._id = uuid
+            self.__from_document(self.__internal_collection.find_one({u'_id': ObjectId(uuid)}))
+            self._internal_id = ObjectId(uuid)
 
     @property
     def uuid(self):
-        return str(self._id)
+        return str(self._internal_id)
 
     def __from_document(self, document: dict):
         if not document:
             return
-        self.__dict__.update(self._decoder().decode(document))
+        self.__dict__.update(self._internal_decoder().decode(document))
 
     def set_member(self, member, value):
         """
@@ -68,8 +68,8 @@ class PersistentObject(object):
         :return: None
         """
         setattr(self, member, value)
-        self.__collection.update_one(
-            {u'_id': self._id},
+        self.__internal_collection.update_one(
+            {u'_id': self._internal_id},
             {u'$set': {'fields.{}'.format(member): value}}
         )
 
@@ -79,10 +79,10 @@ class PersistentObject(object):
         between changed and unchanged fields, but instead replaces whole document
         :return: None
         """
-        if not self._id:
-            self._id = self.__collection.insert_one(self._encoder().encode(self)).inserted_id
+        if not self._internal_id:
+            self._internal_id = self.__internal_collection.insert_one(self._internal_encoder().encode(self)).inserted_id
         else:
-            self.__collection.replace_one({u'_id': self._id}, self._encoder().encode(self))
+            self.__internal_collection.replace_one({u'_id': self._internal_id}, self._internal_encoder().encode(self))
 
     def remove(self):
-        self.__collection.delete_one({u'_id': self._id})
+        self.__internal_collection.delete_one({u'_id': self._internal_id})
