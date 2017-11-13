fe0e0aa7193a213a05066404c480c0db69b59c96
Noah Hummel <strangedev@posteo.net>
2017-11-13 11:24:31 +0100

    ADD businesslogic: users.py - business logic for handling user registration, login

---
+++ websrv-flask/app/businesslogic/users.py
@@ -0,0 +1,55 @@
+import os
+
+import argon2
+from flask import g
+
+import auth
+from framework.exceptions import UserExistsException, BadCredentialsException
+from model.DataClient import DataClient
+
+
+def register(email: str, password: str) -> DataClient:
+    """
+    Registers a new DataClient.
+    Raises UserExistsException, if a user with the given email address already exists.
+    :param email: str The email address of the DataClient
+    :param password: str The password the DataClient will use to login
+    :return: DataClient The resulting DataClient object
+    """
+    client = DataClient.one_from_query({'email': email})
+    if client:
+        raise UserExistsException("User with email address {} already exists.".format(email))
+
+    client = DataClient()
+    client.email = email
+    client.password_salt = os.urandom(g._config['auth.salt_length']).hex()
+    client.password_hash = argon2.argon2_hash(password, client.password_salt)
+
+    return client
+
+
+def login(email: str, password: str) -> str:
+    """
+    Logs a DataClient in by checking if the user exists, matching the password hashes
+    and creating a new session token in memcache if authentication is successful.
+    Returns the session token.
+    Raises SessionExistsException or BadCredentialsException if appropriate.
+    :param email: The email address of the user to log in
+    :param password: The plain text password of the user to log in
+    :return: The session token for the newly created session
+    """
+    client = DataClient.one_from_query({'email': email})
+    if not client:
+        raise BadCredentialsException("User with email {} doesn't exist.".format(email))
+
+    password_hash = argon2.argon2_hash(password, client.password_salt)
+    if password_hash != client.password_hash:
+        raise BadCredentialsException("Tried to login as {}, but password hashes didn't match.".format(email))
+
+    success = False
+    session_token = ""
+    while not success:
+        session_token = os.urandom(g._config['auth.session_token_length']).hex()
+        success = auth.new_session(session_token, client.uuid)
+
+    return session_token
--- websrv-flask/app/framework/exceptions.py
+++ websrv-flask/app/framework/exceptions.py
@@ -8,3 +8,12 @@ class UuidMismatchException(Exception): pass
 
 
 class ObjectDoesntExistException(Exception): pass
+
+
+class BadCredentialsException(Exception): pass
+
+
+class SessionExistsException(Exception): pass
+
+
+class UserExistsException(Exception): pass
