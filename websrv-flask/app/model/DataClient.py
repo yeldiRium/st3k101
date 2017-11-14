e58d295d286b29888e24b1e1cd03fcd8be08fa3a
Noah Hummel <strangedev@posteo.net>
2017-11-14 12:14:38 +0100

    FIX model classes to use correct internal representation

--- websrv-flask/app/model/DataClient.py
+++ websrv-flask/app/model/DataClient.py
@@ -9,57 +9,57 @@ class DataClient(PersistentObject):
     def __init__(self, uuid:str=None):
         super().__init__(uuid)
 
-        self.__email = ""  # type: str
-        self.__activated = False  # type: bool
-        self.__password_salt = ""  # type: str
-        self.__password_hash = ""  # type: str
-        self.__survey_ids = None  # type: List[str]
+        self._email = ""  # type: str
+        self._activated = False  # type: bool
+        self._password_salt = ""  # type: str
+        self._password_hash = ""  # type: str
+        self._survey_ids = None  # type: List[str]
 
     @property
     def email(self) -> str:
-        return self.__email
+        return self._email
 
     @property
     def activated(self):
-        return self.__activated
+        return self._activated
 
     @property
     def password_salt(self) -> str:
-        return self.__password_salt
+        return self._password_salt
 
     @property
     def password_hash(self) -> str:
-        return self.__password_hash
+        return self._password_hash
 
     @property
     def surveys(self) -> List[Survey]:
-        return [Survey(uuid) for uuid in self.__survey_ids]
+        return [Survey(uuid) for uuid in self._survey_ids]
 
     @email.setter
     def email(self, value: str):
         if type(value) != str:
             raise TypeError
-        super().set_member("__email", value)
+        super().set_member("_email", value)
 
     @activated.setter
     def activated(self, value: bool):
         if type(value) != bool:
             raise TypeError
-        super().set_member("__activated", value)
+        super().set_member("_activated", value)
 
     @password_salt.setter
     def password_salt(self, value: str):
         if type(value) != str:
             raise TypeError
-        super().set_member("__password_salt", value)
+        super().set_member("_password_salt", value)
 
     @password_hash.setter
     def password_hash(self, value: str):
         if type(value) != str:
             raise TypeError
-        super().set_member("__password_hash", value)
+        super().set_member("_password_hash", value)
 
     @surveys.setter
     def surveys(self, value:List[Survey]):
         ids = [o.uuid for o in value]
-        super().set_member("__survey_ids", ids)
+        super().set_member("_survey_ids", ids)
--- websrv-flask/app/model/Questionnaire.py
+++ websrv-flask/app/model/Questionnaire.py
@@ -6,26 +6,26 @@ from model.QuestionGroup import QuestionGroup
 
 class Questionnaire(PersistentObject):
     def __init__(self, uuid: str = None):
-        self.__name = ""  # type: str
-        self.__questiongroup_ids = None  # type: List[str]
+        self._name = ""  # type: str
+        self._questiongroup_ids = None  # type: List[str]
 
         super().__init__(uuid)
 
     @property
     def name(self) -> str:
-        return self.__name
+        return self._name
 
     @property
     def questiongroups(self) -> List[QuestionGroup]:
-        return [QuestionGroup(uuid) for uuid in self.__questiongroup_ids]
+        return [QuestionGroup(uuid) for uuid in self._questiongroup_ids]
 
     @name.setter
     def name(self, value: str):
         if type(value) != str:
             raise TypeError
-        super().set_member("__name", value)
+        super().set_member("_name", value)
 
     @questiongroups.setter
     def questiongroups(self, value: List[QuestionGroup]):
         ids = [o.uuid for o in value]
-        super().set_member("__questiongroup_ids", ids)
+        super().set_member("_questiongroup_ids", ids)
