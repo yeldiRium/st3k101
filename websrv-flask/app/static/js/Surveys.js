0a00edbb2d5abb6731c5da3d71ba75cdbcfcb6df
Hannes Leutloff <hannes.leutloff@aol.de>
2017-11-22 13:15:22 +0100

    UPDATE improve survey view in backend

--- websrv-flask/app/static/js/Surveys.js
+++ websrv-flask/app/static/js/Surveys.js
@@ -1,28 +1,45 @@
 angular.module('Surveys', ['ngRoute'])
     .factory('Surveys', ['$http', function($http) {
-        return $http.get('/api/surveys').then(
-            function(result) {
-                return new Promise(function(resolve, reject) {
-                    resolve(result.data);
-                });
-            },
-            function(error) {
-                return new Promise(function(resolve, reject) {
-                    reject(error);
-                });
+        return {
+            query: function() {
+                return $http.get('/api/surveys').then(
+                    function(result) {
+                        return new Promise(function(resolve, reject) {
+                            resolve(result.data);
+                        });
+                    },
+                    function(error) {
+                        return new Promise(function(resolve, reject) {
+                            reject(error);
+                        });
+                    }
+                );
             }
-        )
+        }
     }])
     .controller('SurveysController', ['$scope', 'Surveys',
         function($scope, Surveys) {
-            Surveys.then(
-                function(result) {
-                    $scope.surveys = result;
-                },
-                function(error) {
-                    $scope.error = error;
-                }
-            );
+            /**
+             * Queries surveys and writes them into $scope.surveys.
+             * If something goes wrong, $scope.error will be set.
+             * @returns Promise
+             */
+            $scope.query = function() {
+                return Surveys.query().then(
+                    function(result) {
+                        $scope.surveys = null;
+                        $scope.error = null;
+                        $scope.surveys = result;
+                    },
+                    function(error) {
+                        $scope.surveys = null;
+                        $scope.error = null;
+                        $scope.error = error;
+                    }
+                );
+            };
+            
+            $scope.query();
         }])
     .controller('EditSurveyController', ['$scope',
         function($scope) {
