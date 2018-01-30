98ff5d9422962c76e9898f382d5809535f0cf511
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-30 18:06:59 +0100

    add basic account page wich loads data for currently logged in user

--- websrv-flask/app/static/js/Account.js
+++ websrv-flask/app/static/js/Account.js
@@ -1,19 +1,19 @@
 angular.module('Account', ['ngRoute'])
-    .controller('AccountController', ['$scope',
-        function($scope) {
-
-        }])
-    .factory('Account', ['$http', function($http) {
+    .config(['FlashProvider', function (FlashProvider) {
+        FlashProvider.setTimeout(5000);
+        FlashProvider.setShowClose(true);
+    }])
+    .factory('Account', ['$http', function ($http) {
         return {
-            query: function() {
-                return $http.get('/api/account').then(
-                    function(result) {
-                        return new Promise(function(resolve, reject) {
+            query: function () {
+                return $http.get('/api/account/current').then(
+                    function (result) {
+                        return new Promise(function (resolve, reject) {
                             resolve(result.data);
                         });
                     },
-                    function(error) {
-                        return new Promise(function(resolve, reject) {
+                    function (error) {
+                        return new Promise(function (resolve, reject) {
                             reject(error);
                         });
                     }
@@ -21,8 +21,37 @@ angular.module('Account', ['ngRoute'])
             }
         }
     }])
+    .controller('AccountController', ['$scope', 'Flash', 'Account', 'Locales',
+        function ($scope, Flash, Account, Locales) {
+            $scope.getLocales = function() {
+                return Locales.query().then(
+                    function success(result) {
+                        $scope.locales = result
+                    }, function fail(error) {
+                        Flash.create('danger', error.data.result);
+                    }
+                )
+            };
+
+            $scope.query = function () {
+                return Account.query().then(
+                    function success(result) {
+                        $scope.account = {
+                            uuid: result.uuid,
+                            email: result.fields.email,
+                            locale: result.fields.locale_name
+                        }
+                    }, function fail(error) {
+                        Flash.create('danger', error.data.result);
+                    }
+                )
+            };
+
+            $scope.getLocales();
+            $scope.query();
+        }])
     .config(['$routeProvider', '$locationProvider',
-        function($routeProvider, $locationProvider) {
+        function ($routeProvider, $locationProvider) {
             $locationProvider.hashPrefix('');
             $routeProvider
                 .when('/account', {
