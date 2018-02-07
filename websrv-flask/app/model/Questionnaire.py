ad29044340bfe1b0f76a1db0a77d74c7637bfe75
Hannes Leutloff <hannes.leutloff@aol.de>
2018-02-07 13:30:49 +0100

    add originial_locale field to questionnaire

--- websrv-flask/app/model/Questionnaire.py
+++ websrv-flask/app/model/Questionnaire.py
@@ -30,6 +30,7 @@ class Questionnaire(DataObject):
         questionnaire.question_count = 0
         questionnaire.answer_count = 0
         questionnaire.qac_modules = []
+        questionnaire.original_locale = g._locale.name
         return questionnaire
 
     def add_question_group(self, name):
@@ -124,6 +125,7 @@ class Questionnaire(DataObject):
         self.i15d_description.add_locale(g._locale, description)
 
 
+Questionnaire.original_locale = DataAttribute(Questionnaire, "original_locale")
 Questionnaire.i15d_name = DataPointer(Questionnaire, "i15d_name", I15dString,
                                       serialize=False)
 Questionnaire.i15d_description = DataPointer(Questionnaire, "description",
