3657bc52d96c35fdf962283fb06bb89766ea99bf
Hannes Leutloff <hannes.leutloff@aol.de>
2018-03-06 14:57:38 +0100

    fix default values in QACTextParameter and QACI15dTextParameter

--- websrv-flask/app/model/query_access_control/QACTextParameter.py
+++ websrv-flask/app/model/query_access_control/QACTextParameter.py
@@ -7,10 +7,14 @@ from framework.internationalization import _
 from model.I15dString import I15dString
 from model.query_access_control.QACParameter import QACParameter
 
+
 class QACTextParameter(QACParameter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.text = _("This is a placeholder text, replace it to your liking.")
+        if self.text is None:
+            self.text = _(
+                "This is a placeholder text, replace it to your liking."
+            )
 
 
 QACTextParameter.name = DataString(QACTextParameter, "name")
@@ -21,9 +25,10 @@ QACTextParameter.text = DataAttribute(QACTextParameter, "text")
 class QACI15dTextParameter(QACParameter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.text = I15dString()
-        self.text.set_locale(_("This is a placeholder text, "
-                               "replace it to your liking."), g._locale)
+        if self.text is None:
+            self.text = I15dString()
+            self.text.set_locale(_("This is a placeholder text, "
+                                   "replace it to your liking."), g._locale)
 
 
 QACI15dTextParameter.name = DataString(QACI15dTextParameter, "name")
