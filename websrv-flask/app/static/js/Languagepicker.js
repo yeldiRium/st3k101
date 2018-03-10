a6d5a30b3d0e5223ee4cdb5fe848a85bceda8f77
Hannes Leutloff <hannes.leutloff@aol.de>
2018-03-10 19:58:54 +0100

    remove debug statement

--- websrv-flask/app/static/js/Languagepicker.js
+++ websrv-flask/app/static/js/Languagepicker.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     $(".language_menu li").each(function (index, element) {
         $(element).on("click", function() {
             const languageShort = $(this).children("a").data("target-language");
-            console.log(languageShort);
             window.location.href = window.location.pathname + "?" +
                 $.param({'locale': languageShort});
         })
