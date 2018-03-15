868f809ee570d9cdeebe2266aa43259c5a39d463
Hannes Leutloff <hannes.leutloff@aol.de>
2018-03-15 10:01:07 +0100

    move statistics into separate angular module

--- websrv-flask/app/static/js/App.js
+++ websrv-flask/app/static/js/App.js
@@ -5,8 +5,9 @@ require("radar-chart-d3");
 require("bootstrap");
 require("./Languagepicker");
 require("./Surveys");
+require("./Statistics");
 require("./Account");
 require("./API");
 require("./Utility");
 
-angular.module("App", ["Surveys", "Account", "API", "Utility"]);
+angular.module("App", ["Surveys", "Statistics", "Account", "API", "Utility"]);
