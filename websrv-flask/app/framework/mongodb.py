e5e17b5d07483ff4010b836e5137cd8085a53505
Noah Hummel <strangedev@posteo.net>
2018-03-18 22:41:24 +0100

    FIX: bug in framework/mongodb that spawned too many MongoClients

--- websrv-flask/app/framework/mongodb.py
+++ websrv-flask/app/framework/mongodb.py
@@ -2,11 +2,18 @@ from flask import g
 from pymongo import MongoClient
 
 
-def get_db():
-    client = MongoClient(
-        g._config["MONGODB_HOSTNAME"],
-        username=g._config["MONGODB_USERNAME"],
-        password=g._config["MONGODB_PASSWORD"],
-        authSource=g._config["MONGODB_DATABASE"]
-    )  # the database server
-    return client[g._config["MONGODB_DATABASE"]]  # the database used for persisting objects
+def get_db() -> MongoClient:
+    """
+    Factory method for getting the appropriate MongoClient instance.
+    Also only instantiates one client per request.
+    :return: MongoClient A MongoClient to access the mongodb 
+    """
+    client = getattr(g, "_mongodb", None)
+    if client is None:
+        client = g._mongodb = MongoClient(
+            g._config["MONGODB_HOSTNAME"],
+            username=g._config["MONGODB_USERNAME"],
+            password=g._config["MONGODB_PASSWORD"],
+            authSource=g._config["MONGODB_DATABASE"]
+        )[g._config["MONGODB_DATABASE"]]
+    return client
