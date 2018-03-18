b679bbf30d3dbee506ff6120c3492985817152d6
Noah Hummel <strangedev@posteo.net>
2018-03-18 20:46:46 +0100

    UPDATE docker-rebuild.sh: add second warning

--- docker-rebuild-all.sh
+++ docker-rebuild-all.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 read -p "Warning: This will remove all stopped containers and delete their storage. Continue? " -n 1 -r
-echo    # (optional) move to a new line
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    docker-compose down
-    yes | docker container prune
-    yes | docker volume prune
-    docker-compose build --no-cache
+    read -p "Are you REALLY sure? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        docker-compose down
+        yes | docker container prune
+        yes | docker volume prune
+        docker-compose build --no-cache
+    fi
 fi
