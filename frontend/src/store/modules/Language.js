c7bcd4bb7e72480897d03982fb49095f2612a972
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-04 13:02:35 +0200

    fix LanguageList initialization

--- frontend/src/store/modules/Language.js
+++ frontend/src/store/modules/Language.js
@@ -30,31 +30,35 @@ const store = {
          * usable by the dropdown menu.
          */
         fetchLanguages({commit}) {
-            return Language.all().fork(
-                data => {
+            return Language.all()
+                .mapRej(error => {
                     commit(
                         "setLoadingState", {
                             loadingState: "error",
                             error: error
                         }
                     );
-                },
-                data => {
-                    data = map(
-                        languageTuple => ({
-                            short: languageTuple[0],
-                            long: languageTuple[1]
-                        }),
-                        data
-                    );
-                    commit(
-                        "setLoadingState", {
-                            loadingState: "done"
-                        }
-                    );
-                    commit("setLanguageOptions", data);
-                }
-            );
+                    return error;
+                })
+                .map(
+                    data => {
+                        data = map(
+                            languageTuple => ({
+                                short: languageTuple[0],
+                                long: languageTuple[1]
+                            }),
+                            data
+                        );
+                        commit(
+                            "setLoadingState", {
+                                loadingState: "done"
+                            }
+                        );
+                        commit("setLanguageOptions", data);
+
+                        return data;
+                    }
+                );
         }
     },
     mutations: {
@@ -76,6 +80,9 @@ export default store;
 /**
  * Initializes this store's content.
  *
+ * Since actions encase their return value in a resolved Promise (if the value
+ * isn't a Promise itself), the Promise's resolving value has to be untangled.
+ *
  * @param rootStore The instantiated store.
  * @param namespace The namespace in which this store resides.
  * @return a Future
@@ -84,7 +91,14 @@ export default store;
  * @cancel doesn't exist.
  */
 const initialize = function (rootStore, namespace) {
-    return Future.tryP(() => rootStore.dispatch(`${namespace}/fetchLanguages`));
+    return Future((reject, resolve) => {
+        Future.tryP(
+            () => rootStore.dispatch(`${namespace}/fetchLanguages`)
+        )
+        // The Promise return from the fetchLanguages action will always re-
+        // solve to a Future.
+            .value(resolve);
+    });
 };
 
 export {
