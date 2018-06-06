ff76416267183230de375672cb00aec95a6fb16b
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-06 20:47:26 +0200

    add Dimension and Questionnaire models

---
+++ frontend/src/model/Dimension.js
@@ -0,0 +1,212 @@
+import OwnedResource from "./OwnedResource";
+import Party from "./Party";
+import Question, {
+    ConcreteQuestion,
+    populateOwnedIncomingReferences,
+    ShadowQuestion
+} from "./Question";
+import Future from "fluture";
+
+class Dimension extends OwnedResource {
+    /**
+     * @param {string} href See Resource.
+     * @param {Party}  owner See OwnedResource.
+     * @param {string} name The Dimension's name.
+     * @param {Array.<Question>} questions An Array of all connected Questions.
+     */
+    constructor(href,
+                owner,
+                name,
+                questions) {
+        super(href, owner);
+        this.name = name;
+        this.questions = questions;
+    }
+
+    /**
+     * True, if this Question is a ShadowQuestion.
+     *
+     * @returns {boolean}
+     */
+    get isShadow() {
+        throw new Error("Please override this.");
+    };
+
+    /**
+     * True, if this Question is a ConcreteQuestion.
+     *
+     * @returns {boolean}
+     */
+    get isConcrete() {
+        throw new Error("Please override this.");
+    }
+}
+
+class ConcreteDimension extends Dimension {
+    /**
+     *
+     * @param {string} href See Resource.
+     * @param {Party}  owner See OwnedResource.
+     * @param {string} name See Dimension.
+     * @param {Array.<Question>} questions See Dimension.
+     * @param {number}  incomingReferenceCount Number of references to this
+     *  Dimension.
+     *  This counts references not owned by the current user and can thus be
+     *  bigger than the number of ownedIncomingReferences.
+     * @param {Array.<Resource|ShadowDimension>} ownedIncomingReferences List of
+     *  all references (in form of hrefs or ShadowDimension instances) to this
+     *  Dimension, which the current user owns.
+     */
+    constructor(href,
+                owner,
+                name,
+                questions,
+                incomingReferenceCount,
+                ownedIncomingReferences) {
+        super(href, owner, name, questions);
+
+        this.incomingReferenceCount = incomingReferenceCount;
+        this.ownedIncomingReferences = ownedIncomingReferences;
+    }
+
+    get isShadow() {
+        return false;
+    }
+
+    get isConcrete() {
+        return true;
+    }
+
+    /**
+     * Number of reference to this Dimension, which are owned by the current
+     * user.
+     *
+     * @returns {number}
+     */
+    ownedIncomingReferenceCount() {
+        return this.ownedIncomingReferences.length;
+    }
+}
+
+class ShadowDimension extends Dimension {
+    /**
+     *
+     * @param {string} href See Resource.
+     * @param {Party}  owner See OwnedResource.
+     * @param {string} name See Dimension.
+     * @param {Array.<Question>} questions See Dimension.
+     * @param {Resource|ConcreteDimension} referenceTo Href or instance of the
+     *  referenced Dimension.
+     */
+    constructor(href,
+                owner,
+                name,
+                questions,
+                referenceTo) {
+        super(href, owner, name, questions);
+        this.referenceTo = referenceTo;
+    }
+
+    get isShadow() {
+        return true;
+    }
+
+    get isConcrete() {
+        return false;
+    }
+}
+
+/**
+ * Takes in a Dimension and populates its incoming references field by replacing
+ * all resolvable References with their corresponding ShadowDimension instances.
+ *
+ * Accesses the API to load the Dimensions.
+ *
+ * TODO: implement. currently this replaces all Resource-type references with
+ *       empty objects.
+ *
+ * @param {ConcreteDimension} concreteDimension
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateOwnedIncomingReferences(concreteDimension) {
+    const resolvedShadowDimensionFutures = [];
+
+    // Use basic for loop to easily replace values.
+    for (let i = 0; i < concreteDimension.ownedIncomingReferences.length; i++) {
+        let reference = concreteDimension.ownedIncomingReferences[i];
+
+        if (instanceOf(reference, Resource)) {
+            // TODO: fetch ShadowDimension and replace
+            const shadowDimensionFuture = Future.of({}) // <- replace with actual API call
+                .map(shadowDimension => {
+                    concreteDimension.ownedIncomingReferences[i] =
+                        shadowDimension;
+                    return true;
+                });
+
+            resolvedShadowDimensionFutures.push(shadowDimensionFuture);
+        }
+    }
+    return Future.parallel(Infinity, resolvedShadowDimensionFutures);
+}
+
+/**
+ * If the referenceTo field contains a Resource, it is resolved to a
+ * ConcreteDimension instance. Otherwise it is left as is.
+ *
+ * Accesses the API to load the Dimension.
+ *
+ * If it is invalid or does not resolve to anything, an error is thrown.
+ *
+ * TODO: implement this. currently it replaces the reference with an empty
+ *       object.
+ *
+ * @param {ShadowDimension} shadowDimension
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateReferenceTo(shadowDimension) {
+    if (instanceOf(shadowDimension.referenceTo, Resource)) {
+        // TODO: fetch ConcreteDimension and replace
+        return Future.of({}) // <- replace with actual API call
+            .map(concreteDimension => {
+                shadowDimension.referenceTo = concreteDimension;
+            });
+    }
+    return Future.of(true);
+}
+
+/**
+ * Based on the type of the given Dimension this either populates its incoming
+ * references or its referenceTo field.
+ *
+ * @param {Dimension} dimension
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateDimension(dimension) {
+    if (instanceOf(dimension, ShadowDimension)) {
+        return populateReferenceTo(dimension);
+    }
+    if (instanceOf(dimension, ConcreteDimension)) {
+        return populateOwnedIncomingReferences(dimension);
+    }
+}
+
+export default Dimension;
+
+export {
+    Dimension,
+    ConcreteDimension,
+    ShadowDimension,
+    populateOwnedIncomingReferences,
+    populateReferenceTo,
+    populateDimension
+}
---
+++ frontend/src/model/Questionnaire.js
@@ -0,0 +1,256 @@
+import OwnedResource from "./OwnedResource";
+import {ConcreteDimension, ShadowDimension} from "./Dimension";
+import Future from "fluture";
+import {ConcreteQuestion, ShadowQuestion} from "./Question";
+
+class Questionnaire extends OwnedResource {
+    /**
+     * @param {string} href See Resource.
+     * @param {Party} owner See OwnedResource.
+     * @param {string} name The Questionnaire's name.
+     * @param {string} description A description for the Questionnaire.
+     * @param {boolean} isPublic Whether the Questionnaire can be filled out and
+     *  submitted.
+     * @param {boolean} allowEmbedded Whether the Questionnaire can be used in
+     *  embedded mode in alternate frontends.
+     * @param {string} xapiTarget TODO: find out, what this means exactly
+     * @param {Array.<Dimension>} dimensions The Questionnaire's child dimen-
+     *  sions.
+     */
+    constructor(href,
+                owner,
+                name,
+                description,
+                isPublic,
+                allowEmbedded,
+                xapiTarget,
+                dimensions) {
+        super(href, owner);
+
+        this.name = name;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.allowEmbedded = allowEmbedded;
+        this.xapiTarget = xapiTarget;
+        this.dimensions = dimensions;
+    }
+
+    /**
+     * True, if this Question is a ShadowQuestion.
+     *
+     * @returns {boolean}
+     */
+    get isShadow() {
+        throw new Error("Please override this.");
+    };
+
+    /**
+     * True, if this Question is a ConcreteQuestion.
+     *
+     * @returns {boolean}
+     */
+    get isConcrete() {
+        throw new Error("Please override this.");
+    }
+}
+
+class ConcreteQuestionnaire extends Questionnaire {
+    /**
+     * @param {string} href See Resource.
+     * @param {Party} owner See OwnedResource.
+     * @param {string} name See Questionnaire.
+     * @param {string} description See Questionnaire.
+     * @param {boolean} isPublic See Questionnaire.
+     * @param {boolean} allowEmbedded See Questionnaire.
+     * @param {string} xapiTarget See Questionnaire.
+     * @param {Array.<Dimension>} dimensions See Questionnaire.
+     * @param {number}  incomingReferenceCount Number of references to this
+     *  Questionnaire.
+     *  This counts references not owned by the current user and can thus be
+     *  bigger than the number of ownedIncomingReferences.
+     * @param {Array.<Resource|ShadowQuestionnaire>} ownedIncomingReferences
+     *  List of all references (in form of hrefs or ShadowQuestionnaire instan-
+     *  ces) to this Questionnaire, which the current user owns.
+     */
+    constructor(href,
+                owner,
+                name,
+                description,
+                isPublic,
+                allowEmbedded,
+                xapiTarget,
+                dimensions,
+                incomingReferenceCount,
+                ownedIncomingReferences) {
+        super(href,
+            owner,
+            name,
+            description,
+            isPublic,
+            allowEmbedded,
+            xapiTarget,
+            dimensions);
+
+        this.incomingReferenceCount = incomingReferenceCount;
+        this.ownedIncomingReferences = ownedIncomingReferences;
+    }
+
+    get isShadow() {
+        return false;
+    }
+
+    get isConcrete() {
+        return true;
+    }
+
+    /**
+     * Number of reference to this Questionnaire, which are owned by the current
+     * user.
+     *
+     * @returns {number}
+     */
+    ownedIncomingReferenceCount() {
+        return this.ownedIncomingReferences.length;
+    }
+}
+
+class ShadowQuestionnaire extends Questionnaire {
+    /**
+     * @param {string} href See Resource.
+     * @param {Party} owner See OwnedResource.
+     * @param {string} name See Questionnaire.
+     * @param {string} description See Questionnaire.
+     * @param {boolean} isPublic See Questionnaire.
+     * @param {boolean} allowEmbedded See Questionnaire.
+     * @param {string} xapiTarget See Questionnaire.
+     * @param {Array.<Dimension>} dimensions See Questionnaire.
+     * @param {Resource|ConcreteQuestionnaire} referenceTo Href or instance of the re-
+     *  ferenced Questionnaire.
+     */
+    constructor(href,
+                owner,
+                name,
+                description,
+                isPublic,
+                allowEmbedded,
+                xapiTarget,
+                dimensions,
+                referenceTo) {
+        super(href,
+            owner,
+            name,
+            description,
+            isPublic,
+            allowEmbedded,
+            xapiTarget,
+            dimensions);
+
+        this.referenceTo = referenceTo;
+    }
+
+    get isShadow() {
+        return true;
+    }
+
+    get isConcrete() {
+        return false;
+    }
+}
+
+/**
+ * Takes in a Questionnaire and populates its incoming references field by re-
+ * placing all resolvable References with their corresponding ShadowQuestion-
+ * naire instances.
+ *
+ * Accesses the API to load the Questionnaires.
+ *
+ * TODO: implement. currently this replaces all Resource-type references with
+ *       empty objects.
+ *
+ * @param {ConcreteQuestionnaire} concreteQuestionnaire
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateOwnedIncomingReferences(concreteQuestionnaire) {
+    const resolvedShadowQuestionnaireFutures = [];
+
+    // Use basic for loop to easily replace values.
+    for (let i = 0;
+         i < concreteQuestionnaire.ownedIncomingReferences.length;
+         i++) {
+        let reference = concreteQuestionnaire.ownedIncomingReferences[i];
+
+        if (instanceOf(reference, Resource)) {
+            // TODO: fetch ShadowQuestionnaire and replace
+            const shadowQuestionFuture = Future.of({}) // <- replace with actual API call
+                .map(shadowQuestionnaire => {
+                    concreteQuestionnaire.ownedIncomingReferences[i] =
+                        shadowQuestionnaire;
+                    return true;
+                });
+
+            resolvedShadowQuestionnaireFutures.push(shadowQuestionFuture);
+        }
+    }
+    return Future.parallel(Infinity, resolvedShadowQuestionnaireFutures);
+}
+
+/**
+ * If the referenceTo field contains a Resource, it is resolved to a
+ * ConcreteQuestionnaire instance. Otherwise it is left as is.
+ *
+ * Accesses the API to load the Questionnaire.
+ *
+ * If it is invalid or does not resolve to anything, an error is thrown.
+ *
+ * TODO: implement this. currently it replaces the reference with an empty
+ *       object.
+ *
+ * @param {ShadowQuestionnaire} shadowQuestionnaire
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateReferenceTo(shadowQuestionnaire) {
+    if (instanceOf(shadowQuestionnaire.referenceTo, Resource)) {
+        // TODO: fetch ConcreteQuestionnaire and replace
+        return Future.of({}) // <- replace with actual API call
+            .map(concreteQuestionnaire => {
+                shadowQuestionnaire.referenceTo = concreteQuestionnaire;
+            });
+    }
+    return Future.of(true);
+}
+
+/**
+ * Based on the type of the given Questionnaire this either populates its inco-
+ * ming references or its referenceTo field.
+ *
+ * @param {Questionnaire} questionnaire
+ * @return {Future}
+ * @resolve with nothing, since the change is made in place.
+ * @reject with an API error message.
+ * @cancel TODO: can this actually be cancelled?
+ */
+function populateQuestionnaire(questionnaire) {
+    if (instanceOf(questionnaire, ShadowQuestionnaire)) {
+        return populateReferenceTo(questionnaire);
+    }
+    if (instanceOf(questionnaire, ConcreteQuestionnaire)) {
+        return populateOwnedIncomingReferences(questionnaire);
+    }
+}
+
+export default Questionnaire;
+
+export {
+    Questionnaire,
+    ConcreteQuestionnaire,
+    ShadowQuestionnaire,
+    populateOwnedIncomingReferences,
+    populateReferenceTo,
+    populateQuestionnaire
+}
