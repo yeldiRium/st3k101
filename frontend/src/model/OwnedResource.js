2c3595376a071aacfa0515939d1ccf2b9796ecfb
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-06 19:04:16 +0200

    add OwnedResource and Party model for ownership concept

---
+++ frontend/src/model/OwnedResource.js
@@ -0,0 +1,50 @@
+import Resource from "./Resource";
+import Party from "./Party";
+
+/**
+ * A Resource that can be owned by someone.
+ */
+class OwnedResource extends Resource {
+    /**
+     *
+     * @param {string} href See Resource.
+     * @param {Party} owner
+     */
+    constructor(href,
+                owner) {
+        super(href);
+        this._owner = owner;
+    }
+
+    /**
+     * @returns {Party}
+     */
+    get owner() {
+        return this._owner;
+    }
+
+    /**
+     * @param {Party} party
+     * @return {boolean} True, if the OwnedResource is owned by the given Party.
+     */
+    isOwnedBy(party) {
+        return party.identifiesWith(this._owner);
+    }
+}
+
+/**
+ *
+ * @param {OwnedResource} resource
+ * @param {Party} party
+ * @return {boolean} True, if the resource is owned by the party.
+ */
+function isResourceOwnedBy(resource, party) {
+    return resource.isOwnedBy(party);
+}
+
+export default OwnedResource;
+
+export {
+    OwnedResource,
+    isResourceOwnedBy
+};
