b4df4837e23ca84b5e28d48996f68e8fe9f2a09b
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-06 19:12:07 +0200

    add DataClient for logged in parties

---
+++ frontend/src/model/DataClient.js
@@ -0,0 +1,43 @@
+import Party from "./Party";
+
+class DataClient extends Party {
+    constructor(href,
+                email,
+                language) {
+        super(href);
+        this._email = email;
+        this._language = language;
+    }
+
+    // TODO: refactor email out into own model and/or validate
+
+    /**
+     * @param {string} email
+     */
+    set email(email) {
+        this._email = email;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get email() {
+        return this._email;
+    }
+
+    // TODO: refactor language out into own model and/or validate
+
+    /**
+     * @param {string} language
+     */
+    set language(language) {
+        this._language = language;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get language() {
+        return this._language;
+    }
+}
