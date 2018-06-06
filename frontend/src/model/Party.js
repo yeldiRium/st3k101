4eeccbe3001f3d1ed11dffa27174b8ad8befa4f7
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-06 19:03:51 +0200

    add comparison method for resource identities

---
+++ frontend/src/model/Party.js
@@ -0,0 +1,15 @@
+/**
+ * A Party.
+ * \o/
+ */
+class Party extends Resource {
+    constructor(href) {
+        super(href);
+    }
+}
+
+export default Party;
+
+export {
+    Party
+}
--- frontend/src/model/Resource.js
+++ frontend/src/model/Resource.js
@@ -26,6 +26,28 @@ class Resource {
     get href() {
         return this._href;
     }
+
+    /**     *
+     * @param {Resource} otherResource
+     * @return {boolean} If the Resources are equal.
+     */
+    identifiesWith(otherResource) {
+        return this.href === otherResource.href;
+    }
+}
+
+/**
+ * @param {Resource} aResource
+ * @param {Resource} bResource
+ * @returns {boolean} True, if a identifies with b.
+ */
+function identifiesWith(aResource, bResource) {
+    return aResource.identifiesWith(bResource);
 }
 
 export default Resource;
+
+export {
+    Resource,
+    identifiesWith
+};
