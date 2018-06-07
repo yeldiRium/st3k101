04aad01710dc48612fa77da14b4b1cb56fed7eeb
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-07 12:31:37 +0200

    add very basic Config/Reference component

---
+++ frontend/src/components/Partials/SurveyBase/Config/References.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="config-references">
+        {{ referenceLine }}
+    </div>
+</template>
+
+<script>
+    /**
+     * Displays the references to a ConcreteObject. Works with Questions,
+     * Dimension and Questionnaires by duck-typing.
+     */
+    export default {
+        name: "Config-References",
+        props: {
+            object: {
+                type: Object
+            }
+        },
+        computed: {
+            referenceLine() {
+                return `${this.object.incomingReferenceCount} references.`
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
