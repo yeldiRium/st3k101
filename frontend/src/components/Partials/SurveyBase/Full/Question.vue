df93a48349ba306e52af9b78acbfc8b9deccd190
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-07 12:32:47 +0200

    update FullQuestion component use Config/Reference component add more html structure extend QuestionBase

--- frontend/src/components/Partials/SurveyBase/Full/Question.vue
+++ frontend/src/components/Partials/SurveyBase/Full/Question.vue
@@ -1,28 +1,70 @@
 <template>
-    <div class="question-full">
-        <ListQuestion :question="question" />
-        some more stuff lul.
+    <div class="question-full"
+         :class="classes"
+    >
+        <ListQuestion :question="question"
+                      :mini="true"
+                      :draggable="false"
+                      :disableIcons="true"
+        />
+        <div class="question-full-body">
+            <References :object="question"
+                        v-if="question.isConcrete"
+            />
+        </div>
+        <div class="question-full-footer">
+
+        </div>
     </div>
 </template>
 
 <script>
-    import Question from "../../../../model/Question";
-
+    import QuestionBase from "../QuestionBase";
     import ListQuestion from "../List/Question";
+    import References from "../Config/References";
 
     export default {
-        name: "Full-ListQuestion",
+        name: "Full-Question",
+        extends: QuestionBase,
         components: {
-            ListQuestion
+            ListQuestion,
+            References
         },
-        props: {
-            question: {
-                type: Question
+        computed: {
+            classes() {
+                return {
+                    disabled: this.disabled
+                }
             }
         }
     }
 </script>
 
 <style lang="scss">
+    @import "../../../scss/_variables";
 
+    .list-item.list-question {
+        background-color: $primary;
+
+        &.disabled {
+            background-color: $slightlylight;
+        }
+    }
+
+    .question-full {
+        display: flex;
+        flex-flow: column;
+
+        background-color: $primary-light;
+
+        &.disabled {
+            background-color: $lighter;
+        }
+    }
+
+    .question-full-body {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+    }
 </style>
