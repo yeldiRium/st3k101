df618e451c9ad72635f122f509d9bc836a6df3c4
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-12 09:47:32 +0200

    add QuestionnaireBase component

---
+++ frontend/src/components/Partials/SurveyBase/QuestionnaireBase.vue
@@ -0,0 +1,25 @@
+<template>
+</template>
+
+<script>
+    import Questionnaire from "../../../model/SurveyBase/Questionnaire";
+
+    import SurveyBase from "./SurveyBase";
+
+    /**
+     * Defines some properties and computed propertes for Dimensions.
+     */
+    export default {
+        name: "QuestionnaireBase",
+        extends: SurveyBase,
+        props: {
+            /** @type {Questionnaire} */
+            questionnaire: {
+                type: Questionnaire
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
