caeb9d5c254d971929d0c1f6256d28b2dbb29db9
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-15 15:10:05 +0200

    add LoadingSpinner to new Modal

--- frontend/src/components/Views/Testing/TestLoadingSpinner.vue
+++ frontend/src/components/Views/Testing/TestLoadingSpinner.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="TestLoadingSpinner_container">
         <LoadingSpinner />
+        <Button
+                @click="buttonHandler"
+        >
+            Click to load for 5s.
+        </Button>
+        <LoadingSpinnerModal />
     </div>
 </template>
 
 <script>
     import LoadingSpinner from "../../Partials/LoadingSpinner";
+    import LoadingSpinnerModal from "../../Partials/Modal/Loading";
+    import Button from "../../Partials/Form/Button";
 
     export default {
         name: "TestLoadingSpinner",
         components: {
-            LoadingSpinner
+            LoadingSpinner,
+            LoadingSpinnerModal,
+            Button
+        },
+        methods: {
+            buttonHandler() {
+                this.$modal.show("loading");
+                window.setTimeout(function() {
+                    this.$modal.hide("loading");
+                }.bind(this), 5000);
+            }
         }
     }
 </script>
@@ -23,8 +41,8 @@
         padding: 0;
         margin: 0;
 
-        .loading-spinner {
-            width: 500px;
+        > .loading-spinner {
+            width: 300px;
         }
     }
 </style>
