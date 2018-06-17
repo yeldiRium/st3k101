7c592dccae7c3234c3c3986efddf7c710e25088e
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-17 09:56:45 +0200

    improve session state management

--- frontend/src/store/modules/Session.js
+++ frontend/src/store/modules/Session.js
@@ -1,158 +1,35 @@
-import {path, isNil} from "ramda";
-import Future from "fluture";
+import {isNil, path} from "ramda";
 
 import DataClient from "../../model/DataClient";
 
-import Authentication from "../../api/Authentication";
-import Account from "../../api/Model/Account";
-
-/**
- * TODO: refactor API so that DataClient information is returned directly when
- *       logging in.
- */
 const store = {
     namespaced: true,
     state: {
-        loading: {
-            state: "done",
-            error: null
-        },
         /** @type {DataClient} */
         dataClient: null,
         sessionToken: null
     },
     getters: {
-        loading: state => {
-            return {
-                loadingState: state.loading.state,
-                error: state.loading.error
-            };
-        },
         isLoggedIn(state) {
             return state.sessionToken !== null;
+        },
+        sessionToken(state) {
+            return state.sessionToken;
         },
         dataClient(state) {
             return state.dataClient;
         }
     },
     actions: {
-        /**
-         * Logs in a user with the given credentials, if those match a user.
-         * If the API returns an error, this rejects with the reason.
-         *
-         * @param context
-         * @param email
-         * @param password
-         * @return a Promise.
-         * @resolves with the session object.
-         * @rejects with a reason for failure.
-         */
-        logIn(context, email, password) {
-            context.commit("setLoadingState", {loadingState: "loading"});
-            return Authentication.login(email, password)
-                .mapRej(error => {
-                    context.commit(
-                        "setLoadingState", {
-                            loadingState: "error",
-                            error: error
-                        }
-                    );
-                    return error;
-                })
-                .chain(sessionToken => {
-                    context.commit("setLoadingState", {loadingState: "done"});
-                    context.commit("startSession", {
-                        sessionToken
-                    });
-                })
-                .promise()
+        startSession(context, {sessionToken, dataClient}) {
+            context.commit("startSession", {sessionToken});
+            // TODO: fetch DataClient here
         },
-        /**
-         * Ends the current session.
-         *
-         * @param context
-         * @return a Promise.
-         * @resolves with True, if the logout process was successful.
-         * @rejects with an error message, if not.
-         */
-        logOut(context) {
-            context.commit("setLoadingState", {loadingState: "loading"});
-            return Authentication.logout(context.state.sessionToken)
-                .chain(data => {
-                    context.commit(
-                        "setLoadingState", {
-                            loadingState: "done"
-                        }
-                    );
-                    context.commit("endSession");
-                    return Future.of(true);
-                })
-                .mapRej(error => {
-                    context.commit(
-                        "setLoadingState", {
-                            loadingState: "error",
-                            error: error
-                        }
-                    );
-                    return error;
-                })
-                .promise();
-        },
-        /**
-         * Retrieves the account data for the currently logged in user.
-         * Only works, if a logIn was executed and successful, since it relies
-         * on cookies set by the API.
-         *
-         * @param commit
-         * @return a Promise.
-         * @resolves with the account object.
-         * @rejects with an error message.
-         */
-        fetchAccount({commit}) {
-            if (isNil(state.sessionToken)) {
-                return Promise.reject("Not logged in.");
-            }
-
-            context.commit("setLoadingState", {loadingState: "loading"});
-            return Account.current()
-                .chain(data => {
-                    let email = path(["fields", "email"], data);
-                    let language = path(["fields", "locale_name"], data);
-
-                    context.commit(
-                        "setLoadingState", {
-                            loadingState: "done"
-                        }
-                    );
-                    // TODO: set correct href
-                    commit("setDataClient", new DataClient(
-                        "randomHref",
-                        email,
-                        language
-                    ));
-
-                    return Future.of({
-                        email,
-                        language
-                    });
-                })
-                .mapRej(error => {
-                    context.commit(
-                        "setLoadingState", {
-                            loadingState: "error",
-                            error: error
-                        }
-                    );
-                    return error;
-                })
-                .promise();
+        endSession(context) {
+            context.commit("endSession");
         }
     },
     mutations: {
-        setLoadingState(state, {loadingState, error}) {
-            state.loading.state = loadingState;
-            state.loading.error = error;
-        },
         startSession(state, {sessionToken}) {
             state.sessionToken = sessionToken;
         },
