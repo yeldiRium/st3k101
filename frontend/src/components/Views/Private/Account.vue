ee7f267fa5ac3dee70013093e97db3ef84fd4481
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-17 12:19:57 +0200

    add logut button to account view

--- frontend/src/components/Views/Private/Account.vue
+++ frontend/src/components/Views/Private/Account.vue
@@ -1,9 +1,37 @@
 <template>
-
+    <div class="account">
+        <Button @click="logout">
+            Logout
+        </Button>
+    </div>
 </template>
 
 <script>
+    import {mapGetters} from "vuex";
 
+    import {endSession} from "../../../api2/Authentication";
+
+    import Button from "../../Partials/Form/Button";
+
+    export default {
+        name: "Account",
+        components: {
+            Button
+        },
+        computad: {
+            ...mapGetters("session", ["sessionToken"])
+        },
+        methods: {
+            logout() {
+                this.$load(
+                    endSession(this.sessionToken)
+                ).fork(
+                    console.error,
+                    () => this.$route.push({name: "Authentication"})
+                )
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
