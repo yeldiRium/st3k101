e32f825c0245dcd3fcf23782e60186c19fc279f3
Noah Hummel <strangedev@posteo.net>
2018-06-19 12:59:05 +0200

    FIX repr of ValidationError is enum_field

--- websrv-flask/app/api/v2/schema/fields.py
+++ websrv-flask/app/api/v2/schema/fields.py
@@ -12,12 +12,12 @@ def enum_field(some_enum, **kwargs):
     class EnumFieldClosure(fields.Field):
         def _serialize(self, value, attr, obj):
             if value not in some_enum:
-                raise ValidationError('{} is not a valid value.'.format(value))
+                raise ValidationError('{} is not a valid {}.'.format(value, some_enum.__name__))
             return {'name': value.name, 'value': value.value}
 
         def _deserialize(self, value, attr, data):
             if not is_in_enum(value):
-                raise ValidationError('{} is not a valid language.'.format(value))
+                raise ValidationError('{} is not a valid {}.'.format(value, some_enum.__name__))
             return some_enum[value['name']]
 
     return EnumFieldClosure(**kwargs)
