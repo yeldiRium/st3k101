3d701f6e4cf4e8e8eb7362106cfdbeb7bff8f1f2
Noah Hummel <strangedev@posteo.net>
2018-06-19 14:46:24 +0200

    FIX __init__ of RESTFulSchema in wrong class

--- websrv-flask/app/api/v2/schema/dataclient.py
+++ websrv-flask/app/api/v2/schema/dataclient.py
@@ -1,6 +1,3 @@
-from typing import Type
-
-from flask_restful import Resource
 from marshmallow import fields
 
 from api.v2.schema import RESTFulSchema
@@ -16,9 +13,3 @@ class DataClientSchema(RESTFulSchema):
     language = enum_field(BabelLanguage)
     roles = fields.List(enum_field(Role))
     password = fields.String(load_only=True, required=True)
-
-    def __init__(self, resource: Type[Resource], *args, **kwargs):
-        if 'context' not in kwargs:
-            kwargs['context'] = dict()
-        kwargs['context'] = {**kwargs['context'], **{'resource': resource}}
-        super(RESTFulSchema, self).__init__(*args, **kwargs)
