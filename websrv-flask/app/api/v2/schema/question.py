847763ef3d97fab8a77570ce634203bc878ac030
Noah Hummel <n.p.hummel@gmail.com>
2018-06-22 19:32:41 +0200

    ADD Questionnaire API

--- websrv-flask/app/api/v2/schema/question.py
+++ websrv-flask/app/api/v2/schema/question.py
@@ -1,6 +1,7 @@
 from marshmallow import fields
 
 from api.v2.schema.survey_base import SurveyBaseSchema
+from model.SQLAlchemy.models.Question import ShadowQuestion
 
 __author__ = "Noah Hummel"
 
@@ -8,3 +9,7 @@ __author__ = "Noah Hummel"
 class QuestionSchema(SurveyBaseSchema):
     text = fields.String(required=True)
     range = fields.Integer(default=10)
+    shadow = fields.Method('is_shadow')
+
+    def is_shadow(self, obj):
+        return isinstance(obj, ShadowQuestion)
