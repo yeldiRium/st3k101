9c2bcf0d6fef9bf75e942ddb96919bed13ba02ce
Noah Hummel <n.p.hummel@gmail.com>
2018-06-22 19:31:29 +0200

    FIX inconsitencies in DataClient API, ADD verification_token to DataClient model

--- websrv-flask/app/api/v2/dataclient.py
+++ websrv-flask/app/api/v2/dataclient.py
@@ -32,24 +32,35 @@ class DataClientResource(Resource):
             dataclient = DataClient.query.get(item_id)
             if dataclient is None:
                 abort(404)
-            return schema.dump(dataclient).data
+            return self.dump(schema, dataclient)
 
         else:
-            return schema.dump(current_user()).data
+            return self.dump(schema, current_user())
 
     def post(self, item_id=None):
         if item_id is not None:
             abort(400, message='Can\'t specify item id on creation.')
 
-        schema = DataClientSchema(DataClientResource)
+        schema = DataClientSchema()
         data, errors = schema.load(request.json)
-        if errors:
-            return errors, 400
+        for err in errors:
+            if err in data:
+                del data[err]
+        if not data:
+            return {
+                'message': 'DataClient not created. Some errors occurred.',
+                'errors': errors
+            }, 400
 
         try:
             dataclient = users.register(data['email'], data['password'])
         except UserExistsException:
-            return {'email': ['An user with that email address already exists.']}, 409
+            return {
+                'message': 'DataClient not created. Some errors occurred.',
+                'errors': {
+                    'email': ['An user with that email address already exists.']
+                }
+            }, 409
 
         del data['password']
         for k, v in data.items():
@@ -78,16 +89,18 @@ class DataClientResource(Resource):
             }
 
         else:
-            dataclient_data = DataClientSchema().dump(current_user()).data
+            schema = DataClientSchema()
+            dataclient_data = self.dump(schema, current_user())
             db.session.delete(current_user())
             db.session.commit()
+            users.logout()
             return {
                 'message': 'DataClient removed',
                 'data_client': dataclient_data
             }
 
     @needs_minimum_role(Role.User)
-    def put(self, item_id=None):
+    def patch(self, item_id=None):
         if item_id is not None and item_id != current_user().id:
             if not current_has_minimum_role(Role.Admin):
                 abort(403, message='You are not allowed to modify other user\'s'
@@ -98,35 +111,52 @@ class DataClientResource(Resource):
         else:
             dataclient = current_user()
 
-        schema = DataClientSchema(DataClientResource, partial=True)
+        schema = DataClientSchema(partial=True)
         data, errors = schema.load(request.json)
         for err in errors:
             if err in data:
                 del data[err]
 
-        if 'roles' in data:
-            if not current_has_minimum_role(Role.Admin):
-                errors['roles'] = ['You are note allowed to change user\'s roles']
-            dataclient.update_roles(data['roles'])
-            del data['roles']
-        for k, v in data.items():
-            setattr(dataclient, k, v)
-        db.session.commit()
-
         if data:
+            if 'roles' in data:
+                if not current_has_minimum_role(Role.Admin):
+                    errors['roles'] = ['You are note allowed to change user\'s roles']
+                else:
+                    dataclient.update_roles(data['roles'])
+                del data['roles']
+            for k, v in data.items():
+                setattr(dataclient, k, v)
+            db.session.commit()
+
             response = {
                 'message': 'DataClient updated.',
-                'data_client': schema.dump(dataclient).data
+                'data_client': self.dump(schema, dataclient)
             }
+            status = 200
         else:
             response = {
                 'message': 'DataClient not updated.'
             }
+            status = 400
         if errors:
             response['message'] += ' Some errors occurred.'
             response['errors'] = errors
-        return response
+        return response, status
 
 
-api.add_resource(DataClientResource, '/api/dataclient',
-                 '/api/dataclient/<int:item_id>', endpoint='dataclient')
+class DataClientVerificationResource(Resource):
+    def get(self, token: str=None):
+        dataclient = DataClient.query.filter_by(verification_token=token).first()
+        if dataclient is None:
+            abort(404)
+        dataclient.verified = True
+        dataclient.verification_token = None
+        db.session.commit()
+        return  # TODO redirect
+
+
+api.add_resource(DataClientResource,
+                 '/api/dataclient/<int:item_id>',
+                 '/api/dataclient')
+api.add_resource(DataClientVerificationResource,
+                 '/api/dataclient/verify/<token>')
