c1bf1be6239214c1a31bbc376c9848d8aeab5062
Noah Hummel <n.p.hummel@gmail.com>
2018-06-22 19:29:16 +0200

    changed enum item serialization (name, value) -> (item_id, value)

--- websrv-flask/app/api/v2/schema/fields.py
+++ websrv-flask/app/api/v2/schema/fields.py
@@ -1,23 +1,38 @@
 from marshmallow import fields, ValidationError
 
+from utils import check_color
+
 __author__ = "Noah Hummel"
 
 
 def enum_field(some_enum, **kwargs):
 
     def is_in_enum(v):
-        return any((v['name'] == i.name and v['value'] == i.value)
+        return any((v['item_id'] == i.name and v['value'] == i.value)
                    for i in some_enum)
 
     class EnumFieldClosure(fields.Field):
         def _serialize(self, value, attr, obj):
             if value not in some_enum:
                 raise ValidationError('{} is not a valid {}.'.format(value, some_enum.__name__))
-            return {'name': value.name, 'value': value.value}
+            return {'item_id': value.name, 'value': value.value}
 
         def _deserialize(self, value, attr, data):
+            if 'item_id' not in value or 'value' not in value:
+                raise ValidationError('Invalid enumerator format.')
             if not is_in_enum(value):
                 raise ValidationError('{} is not a valid {}.'.format(value, some_enum.__name__))
-            return some_enum[value['name']]
+            return some_enum[value['item_id']]
 
     return EnumFieldClosure(**kwargs)
+
+
+class HexColor(fields.Field):
+    def _serialize(self, value, attr, obj):
+        return value
+
+    def _deserialize(self, value, attr, data):
+        try:
+            check_color(value)
+        except ValueError as e:
+            raise ValidationError(*e.args)
