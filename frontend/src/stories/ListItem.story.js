9dbaaa80830285bdcecc39524295e7fdcecb5a23
Hannes Leutloff <hannes.leutloff@aol.de>
2018-06-25 21:04:50 +0200

    add more ListItem stories

--- frontend/src/stories/ListItem.story.js
+++ frontend/src/stories/ListItem.story.js
@@ -1,13 +1,20 @@
 import {storiesOf} from "@storybook/vue";
 import {action} from "@storybook/addon-actions";
-import {withKnobs} from "@storybook/addon-knobs";
+import {boolean, text, withKnobs} from "@storybook/addon-knobs";
 
 import ListItem from "../components/Partials/List/Item";
+
+import IconReorder from "../assets/icons/baseline-reorder-24px.svg";
+import IconExpandMore from "../assets/icons/baseline-expand_more-24px.svg";
 
 storiesOf('ListItem', module)
     .addDecorator(withKnobs)
     .addDecorator(() => ({
-        template: '<div style="display: flex; justify-content: center"><story/></div>'
+        template: '<div style="display: flex; justify-content: center">' +
+        '   <div style="min-width: 300px">' +
+        '       <story/>' +
+        '   </div>' +
+        '</div>'
     }))
     .add('basic', () => ({
         components: {
@@ -15,10 +22,60 @@ storiesOf('ListItem', module)
         },
         data() {
             return {
-                text: "This is a ListElement",
+                text: "This is a basic, editable ListElement"
             }
         },
-        template: '<ListItem v-model="text" @input="action" style="min-width: 300px"/>',
+        template: '<ListItem v-model="text" @input="action"/>',
+        methods: {
+            action: action("text-edited")
+        }
+    }))
+    .add('basic with subtext', () => ({
+        components: {
+            ListItem
+        },
+        data() {
+            return {
+                text: "This is a basic, editable ListElement",
+                subtext: "This is the element's subtext."
+            }
+        },
+        template: '<ListItem v-model="text" :subtext="subtext" @input="action"/>',
+        methods: {
+            action: action("text-edited")
+        }
+    }))
+    .add('generic', () => ({
+        components: {
+            ListItem,
+            IconExpandMore,
+            IconReorder
+        },
+        data() {
+            return {
+                text: "This is a basic, editable ListElement",
+                subtext: text("Subtext", "This is the element's subtext."),
+                disabled: boolean("Disabled", false),
+                mini: boolean("Mini", false),
+                icons: boolean("Icons", true),
+                ellipseText: boolean("Ellipse Text", true),
+                ellipseSubText: boolean("Ellipse Subtext", true),
+                editableText: boolean("Editable Text", true)
+            }
+        },
+        template: '<ListItem v-model="text"' +
+        '                    :subtext="subtext"' +
+        '                    :disabled="disabled"' +
+        '                    :mini="mini"' +
+        '                    :icons="icons"' +
+        '                    :ellipseText="ellipseText"' +
+        '                    :ellipseSubText="ellipseSubText"' +
+        '                    :editableText="editableText"' +
+        '                    @input="action"' +
+        '          >' +
+        '              <IconExpandMore class="list-item__icon"/>' +
+        '              <IconReorder class="list-item__icon"/>' +
+        '          </ListItem>',
         methods: {
             action: action("text-edited")
         }
