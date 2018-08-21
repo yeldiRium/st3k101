1b0c1691b5643fd55398d0621248247e3d62b73f
Noah Hummel <n.p.hummel@gmail.com>
2018-08-21 18:05:30 +0200

    ADD utils for manipulating the request context when running from cli

---
+++ websrv-flask/app/utils/flask.py
@@ -0,0 +1,28 @@
+from flask import g
+from contextlib import contextmanager
+
+from framework.internationalization.babel_languages import BabelLanguage
+
+__author__ = "Noah Hummel"
+
+def setup_app_context(app, language=None, user=None):
+    """
+    Sets request-global variables needed for the app to function.
+    Normally, these variables are set before_request. This
+    method can be used to populate the app context with sane dummy
+    values. Used for creating an app context in cli commands.
+    """
+    g._config = app.config
+    if language is None or language not in BabelLanguage:
+        g._language = app.config['BABEL_DEFAULT_LOCALE']
+    else:
+        g._language = language
+    g._current_user = user  # TODO: what should be the default value?
+
+
+@contextmanager
+def context_language(language: BabelLanguage):
+    previous_language = g._language
+    g._language = language
+    yield
+    g._language = previous_language
