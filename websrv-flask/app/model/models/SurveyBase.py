2502e0a77c63cbfd47099b5afa96ddf7242ef321
Noah Hummel <n.p.hummel@gmail.com>
2018-08-21 11:42:16 +0200

    FIX: generate reference_id when first read, instead of at construction, to let data dependencies become available

--- websrv-flask/app/model/models/SurveyBase.py
+++ websrv-flask/app/model/models/SurveyBase.py
@@ -38,6 +38,10 @@ class SurveyBase(OwnershipBase):
 
     @property
     def reference_id(self) -> str:
+        # set initial reference_id if name already present
+        if not self._reference_id:
+            if self.name is not None and len(self.name) > 0:
+                self._reference_id = self.generate_reference_id()
         return self._reference_id
 
     @reference_id.setter
@@ -138,10 +142,6 @@ class SurveyBase(OwnershipBase):
     def __init__(self, **kwargs):
         super(SurveyBase, self).__init__(**kwargs)
         self.owners.append(current_user())
-        # set initial reference_id if name already present
-        if not self._reference_id:
-            if self.name is not None and len(self.name) > 0:
-                self._reference_id = self.generate_reference_id()
 
     def accessible_by(self, party: "Party") -> bool:
         """
