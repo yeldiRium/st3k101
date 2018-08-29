bea54a93672922c846246bc42e84add7548bb33a
Noah Hummel <n.p.hummel@gmail.com>
2018-08-29 14:58:08 +0200

    FIX activity registry

--- websrv-flask/app/framework/xapi/XApiActivities.py
+++ websrv-flask/app/framework/xapi/XApiActivities.py
@@ -9,15 +9,22 @@ class XApiActivities(Enum):
     }
 
     Dimension = {
-        "id": "http://fantasy.land/dimension",
+        "type": "http://fantasy.land/dimension",
         "description": {
             "en-US": "This is a particular scale of a survey, it usually contains multiple questions."
         }
     }
 
     Questionnaire = {
-        "id": "http://id.tincanapi.com/activitytype/survey",
+        "type": "http://id.tincanapi.com/activitytype/survey",
         "description": {
             "en-US": "This is a survey hosted at st3k101."
+        }
+    }
+
+    Login = {
+        "type": "http://activitystrea.ms/schema/1.0/page",
+        "description": {
+            "en-US": "This is the log in page of the st3k101 survey tool."
         }
     }
