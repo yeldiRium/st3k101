b3bac3bf51b75ec363b0656443b7ffc91a99190f
Noah Hummel <n.p.hummel@gmail.com>
2018-08-30 16:33:17 +0200

    ADD xapi signals

--- websrv-flask/app/framework/signals/__init__.py
+++ websrv-flask/app/framework/signals/__init__.py
@@ -12,3 +12,8 @@ item_removed = app_signals.signal('item_removed')
 questionnaire_removed = app_signals.signal('questionnaire_removed')
 
 SIG_LOGGED_IN = app_signals.signal('logged_in')
+SIG_ANSWER_SUBMITTED = app_signals.signal('answer_submitted')
+SIG_REFERENCE_ID_UPDATED = app_signals.signal('reference_id_updated')
+SIG_QUESTIONNAIRE_PUBLISHED = app_signals.signal('questionnaire_published')
+SIG_QUESTIONNAIRE_UNPUBLISHED = app_signals.signal('questionnaire_unpublished')
+SIG_SURVEY_CONCLUDED = app_signals.signal('questionnaire_')
