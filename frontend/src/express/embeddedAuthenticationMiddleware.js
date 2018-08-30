694b043e48b8e3098d9dccb83383c31e5131f05e
Hannes Leutloff <hannes.leutloff@aol.de>
2018-08-30 23:02:21 +0200

    check if key is valid

--- frontend/src/express/embeddedAuthenticationMiddleware.js
+++ frontend/src/express/embeddedAuthenticationMiddleware.js
@@ -1,22 +1,31 @@
 import R from "ramda";
 
+import {getCurrentDataClient} from "../api/Authentication";
+
 /**
  * @param req Express request.
  * @param res Express response.
  * @param next Callback for next middleware.
  */
 const embeddedAuthenticationMiddleware = function (req, res, next) {
-  if (req.method === "POST") {
     // No key given, no access granted.
-    if (R.not(R.has("key", req.body))) {
-      return next();
+    if (req.method === "POST" && R.has("key", req.body)) {
+        const key = req.body.key;
+
+        return getCurrentDataClient(key).fork(
+            error => {
+                console.error(error);
+                next();
+            },
+            /** @type {DataClient} */
+            dataClient => {
+                // A successful request means that the key is valid.
+                // TODO: set cookie with key for the frontend.
+                next();
+            }
+        );
     }
-
-    const key = req.body.key;
-
-
-  }
-  next();
+    next();
 };
 
 export default embeddedAuthenticationMiddleware;
