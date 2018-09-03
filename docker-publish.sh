2e1087611bb481e3202dad8444bc62bc0316bd46
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-03 20:00:43 +0200

    treat registry as parameter and expand usage information

--- docker-publish.sh
+++ docker-publish.sh
@@ -4,7 +4,14 @@ dockerfile=$1
 registry=$2
 folder=$(dirname $dockerfile)
 service=$(basename $folder)
+version=$(cat "$folder/Version")
+sha=$(git rev-parse HEAD)
 
-name="$registry/uc/efla-$service:latest"
+name="$registry/uc/efla-$service"
+latest="$name:latest"
+version="$name:$version"
+sha="$name:$sha"
 
-docker push $name
+docker push "$latest"
+docker push "$version"
+docker push "$sha"
