4b7aec58d91187e3dbd69c3a6b82c7ebd22c1a6c
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-03 20:00:27 +0200

    push all tags to docker repository

--- build.sh
+++ build.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-if [ -z ${DOCKER_REGISTRY} ]; then
-	echo "Environment Variable DOCKER_REGISTRY must be set."
-	exit 1
-fi
-
-docker_registry=${DOCKER_REGISTRY}
-
 info_message () {
 	echo    "Usage:"
-	echo -e "\tbuild command"
+	echo -e "\tbuild command [registry]"
 	echo    ""
 	echo    "Commands:"
-	echo -e "\tbuild   Build all images in this repository."
+	echo -e "\tbuild     Build images."
+	echo -e "\ttest      Test images."
+	echo -e "\tpublish   Publish images to registry."
+	echo -e "\trun       Build, test and publish."
+	echo    ""
+	echo -e "Parameters: "
+	echo -e "\tregistry  Docker registry in the form hostname[:port]."
+	echo -e "\t          Required by build, publish and run."
+}
+
+check_registry () {
+	if [ -z $docker_registry ]; then
+		echo "Parameter registry is missing!"
+		echo ""
+		info_message
+		exit 1
+		fi
 }
 
 build () {
@@ -27,11 +36,18 @@ publish () {
 	find . -name Dockerfile -exec ./docker-publish.sh {} $docker_registry \;
 }
 
+docker_registry=$2
 
 case $1 in
 	build) build ;;
 	test) test ;;
-	publish) publish ;;
-	run) build && test && publish ;;
+	publish)
+		check_registry
+		publish
+	;;
+	run)
+		check_registry
+		build && test && publish
+	;;
 	*) info_message ;;
 esac
