32161043a23c0b71405b06fc0854f97a85c975f2
Noah Hummel <n.p.hummel@gmail.com>
2018-09-06 14:53:19 +0200

    ADD XApiPublisher, untested

--- websrv-flask/app/framework/xapi/signals.py
+++ websrv-flask/app/framework/xapi/signals.py
@@ -1,5 +1,5 @@
 from framework.signals import SIG_LOGGED_IN, SIG_ANSWER_SUBMITTED
-from framework.xapi.publisher import publish_xapi_statement, void_xapi_statement
+from framework.xapi.publisher import XApiPublisher
 from framework.xapi.statements.XApiAnswerSubmittedStatement import XApiAnswerSubmittedStatement
 from framework.xapi.statements.XApiLoggedInStatement import XApiLoggedInStatement
 from model.models.DataClient import DataClient
@@ -12,20 +12,21 @@ __author__ = "Noah Hummel"
 @SIG_LOGGED_IN.connect
 def publish_logged_in_xapi_statement(sender: DataClient):
     statement = XApiLoggedInStatement(sender)
-    publish_xapi_statement(statement)
+    XApiPublisher.get_instance().enqueue(statement)
 
 
 @SIG_ANSWER_SUBMITTED.connect
-def publish_answer_submitted_statement(
+def publish_answer_submitted_xapi_statement(
         sender: DataSubject,
         question: Question,
         value: int
 ):
     previous_results = filter(lambda r: r.verified, question.get_results_by_subject(sender))
+    receiver = question.dimension.questionnaire.xapi_target
     for previous_result in previous_results:
         previous_statement = previous_result.xapi_statement
         if previous_statement is not None and 'id' in previous_statement:
-            void_xapi_statement(statement_id=previous_statement['id'])
+            XApiPublisher.get_instance().void(previous_statement['id'], receiver)
 
     statement = XApiAnswerSubmittedStatement(sender, question, value)
-    publish_xapi_statement(statement)
+    XApiPublisher.get_instance().enqueue(statement, receiver)
