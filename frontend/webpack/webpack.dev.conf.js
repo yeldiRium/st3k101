124c7824a52bdc712478ba74d358ecd2eb210d84
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-15 11:53:56 +0200

    clean up packages

--- frontend/webpack/webpack.dev.conf.js
+++ frontend/webpack/webpack.dev.conf.js
@@ -7,7 +7,6 @@ const path = require('path');
 const baseWebpackConfig = require('./webpack.base.conf');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const portfinder = require('portfinder');
 
 const HOST = process.env.HOST;
