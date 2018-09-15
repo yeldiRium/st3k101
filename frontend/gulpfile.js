4c771b1e553666ce5032c175b07f5b95f0a23304
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-15 11:55:47 +0200

    optimize gulpfile; don't exit on error when watching

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -4,6 +4,26 @@ const rm = require('rimraf');
 const webpack = require("webpack");
 const config = require('./config');
 
+function echoStats(stats, exitOnError = true) {
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n');
+
+    if (stats.hasErrors()) {
+        console.log('  Build failed with errors.\n');
+        if (exitOnError) {
+            process.exit(1);
+        }
+    } else {
+        console.log('  Build complete.\n');
+    }
+
+}
+
 gulp.task("cleanDist", done => {
     rm(config.build.distPath, done);
 });
@@ -12,20 +32,9 @@ gulp.task("build", ["cleanDist"], done => {
     const webpackConfig = require('./webpack/webpack.prod.conf');
     webpack(webpackConfig, (err, stats) => {
         if (err) throw err;
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n');
 
-        if (stats.hasErrors()) {
-            console.log('  Build failed with errors.\n');
-            process.exit(1);
-        }
+        echoStats(stats);
 
-        console.log('  Build complete.\n');
         console.log(
             '  Tip: built files are meant to be served over an HTTP server.\n' +
             '  Opening index.html over file:// won\'t work.\n'
@@ -43,19 +52,7 @@ gulp.task("dev", ["cleanDist"], done => {
     );
     webpack(webpackConfig, (err, stats) => {
         if (err) throw err;
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false,
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n');
 
-        if (stats.hasErrors()) {
-            console.log('  Build failed with errors.\n');
-            process.exit(1);
-        }
-
-        console.log('  Build complete.\n');
+        echoStats(stats, false);
     });
 });
