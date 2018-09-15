59f34bc0da50ef0cde7452e04f7d4908d5aef3a4
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-15 11:06:59 +0200

    finish build process with watching for dev

--- frontend/build/webpack.dev.conf.js
+++ frontend/build/webpack.dev.conf.js
@@ -13,7 +13,7 @@ const portfinder = require('portfinder');
 const HOST = process.env.HOST;
 const PORT = process.env.PORT && Number(process.env.PORT);
 
-const devWebpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
     module: {
         rules: utils.styleLoaders({
             sourceMap: config.dev.cssSourceMap,
@@ -23,32 +23,12 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     // cheap-module-eval-source-map is faster for development
     devtool: config.dev.devtool,
 
-    // these devServer options should be customized in /config/index.js
-    devServer: {
-        clientLogLevel: 'warning',
-        historyApiFallback: {
-            rewrites: [
-                {
-                    from: /.*/,
-                    to: path.posix.join(config.dev.distPublicPath, 'index.html')
-                },
-            ],
-        },
-        hot: true,
-        contentBase: false, // since we use CopyWebpackPlugin.
-        compress: true,
-        host: HOST || config.dev.host,
-        port: PORT || config.dev.port,
-        open: config.dev.autoOpenBrowser,
-        overlay: config.dev.errorOverlay
-            ? {warnings: false, errors: true}
-            : false,
-        publicPath: config.dev.distPublicPath,
-        quiet: true, // necessary for FriendlyErrorsPlugin
-        watchOptions: {
-            poll: config.dev.poll,
-        }
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
     },
+
     plugins: [
         new webpack.DefinePlugin({
             "process.env": require('../config/dev.env')
@@ -58,8 +38,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         new webpack.NoEmitOnErrorsPlugin(),
         // https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
+            template: config.build.indexTemplate,
+            filename: config.build.indexTarget,
             inject: true
         }),
         // copy custom static assets
@@ -71,30 +51,4 @@ const devWebpackConfig = merge(baseWebpackConfig, {
             }
         ])
     ]
-});
-
-module.exports = new Promise((resolve, reject) => {
-    portfinder.basePort = process.env.PORT || config.dev.port;
-    portfinder.getPort((err, port) => {
-        if (err) {
-            reject(err)
-        } else {
-            // publish the new Port, necessary for e2e tests
-            process.env.PORT = port;
-            // add port to devServer config
-            devWebpackConfig.devServer.port = port;
-
-            // Add FriendlyErrorsPlugin
-            devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-                compilationSuccessInfo: {
-                    messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
-                },
-                onErrors: config.dev.notifyOnErrors
-                    ? utils.createNotifierCallback()
-                    : undefined
-            }));
-
-            resolve(devWebpackConfig)
-        }
-    })
 });
