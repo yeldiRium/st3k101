bdb48425f3ef17f38545c13aac73368e46c34e46
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-15 09:57:32 +0200

    remove a lot of obsolete shit

--- frontend/src/stories/index.js
+++
@@ -1,9 +0,0 @@
-require("./Button.story");
-require("./EditableText.story");
-require("./ToggleButton.story");
-require("./ListItem.story");
-require("./LanguagePicker.story");
-
-require("./Question.story");
-require("./Dimension.story");
-require("./Questionnaire.story");
