079b67fae38a478d5d5c41075fff7754713d5656
Noah Hummel <n.p.hummel@gmail.com>
2018-09-18 11:50:48 +0200

    ADD loading of questionnaire to EmbeddedSurveyForSubmission.vue

--- frontend/src/components/Views/Embedded/EmbeddedSurveyForSubmission.vue
+++ frontend/src/components/Views/Embedded/EmbeddedSurveyForSubmission.vue
@@ -1,10 +1,60 @@
 <template>
-    <p>Hello world!</p>
+    <div>
+        <div class="survey"
+             v-if="submissionQuestionnaire !== null"
+        >
+            <p>{{submissionQuestionnaire.name}}</p>
+        </div>
+        <div class="error"
+             v-if="error !== null"
+        >
+            <p>{{error.message}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
+    import * as Future from "fluture";
+
     export default {
-        name: "EmbeddedSurveyForSubmission"
+        name: "EmbeddedSurveyForSubmission",
+        data() {
+            return {
+                submissionQuestionnaire: null,
+                selectedDimensionId: null,
+                error: null
+            }
+        },
+        methods: {
+            loadQuestionnaire(language = null) {
+                if (typeof ltiLaunchParameters === 'undefined') {
+                    return Future.reject("No LTI parameters present!");
+                }
+                return this.$load(
+                    this.$store.dispatch(
+                        "submission/fetchSubmissionQuestionnaireById",
+                        {
+                            id: ltiLaunchParameters.questionnaireId,
+                            language: language
+                        }
+                    )
+                );
+            }
+        },
+        created: function () {
+            this.loadQuestionnaire()
+                .fork(
+                    error => {
+                        this.error = error;
+                    },
+                    questionnaire => {
+                        this.submissionQuestionnaire = questionnaire;
+                        if (questionnaire.dimensions.length > 0) {
+                            this.selectedDimensionId = questionnaire.dimensions[0].id;
+                        }
+                    }
+                );
+        }
     }
 </script>
 
