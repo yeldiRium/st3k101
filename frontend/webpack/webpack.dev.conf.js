a858430b2555c10545cf8cef26db4382c7eb0193
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-20 13:42:01 +0200

    apply eslint to webpack and build configs

--- frontend/webpack/webpack.dev.conf.js
+++ frontend/webpack/webpack.dev.conf.js
@@ -1,53 +1,53 @@
-'use strict';
-const utils = require('./utils');
-const webpack = require('webpack');
-const config = require('../config');
-const merge = require('webpack-merge');
-const path = require('path');
-const baseWebpackConfig = require('./webpack.base.conf');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const portfinder = require('portfinder');
+"use strict";
+const utils = require("./utils");
+const webpack = require("webpack");
+const config = require("../config");
+const merge = require("webpack-merge");
+const path = require("path");
+const baseWebpackConfig = require("./webpack.base.conf");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const portfinder = require("portfinder");
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT && Number(process.env.PORT);
 
 module.exports = merge(baseWebpackConfig, {
-    module: {
-        rules: utils.styleLoaders({
-            sourceMap: config.dev.cssSourceMap,
-            usePostCSS: true
-        })
-    },
-    // cheap-module-eval-source-map is faster for development
-    devtool: config.dev.devtool,
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      usePostCSS: true
+    })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: config.dev.devtool,
 
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000
-    },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
 
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": require('../config/dev.env')
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-        new webpack.NoEmitOnErrorsPlugin(),
-        // https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            template: config.build.indexTemplate,
-            filename: config.build.indexTarget,
-            inject: true
-        }),
-        // copy custom static assets
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, '../static'),
-                to: config.dev.distAssetsPath,
-                ignore: ['.*']
-            }
-        ])
-    ]
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": require("../config/dev.env")
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: config.build.indexTemplate,
+      filename: config.build.indexTarget,
+      inject: true
+    }),
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "../static"),
+        to: config.dev.distAssetsPath,
+        ignore: [".*"]
+      }
+    ])
+  ]
 });
