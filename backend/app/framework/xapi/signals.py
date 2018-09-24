953b8efeade699c8a6a96947d3943ac05389d3fa
Noah Hummel <n.p.hummel@gmail.com>
2018-09-24 20:21:27 +0200

    REFACTOR publish_answer_submitted_xapi_statement

--- backend/app/framework/xapi/signals.py
+++ backend/app/framework/xapi/signals.py
@@ -1,32 +1,35 @@
+from typing import Union
+
 from framework.signals import SIG_LOGGED_IN, SIG_ANSWER_SUBMITTED
 from framework.xapi.publisher import XApiPublisher
 from framework.xapi.statements.XApiAnswerSubmittedStatement import XApiAnswerSubmittedStatement
 from framework.xapi.statements.XApiLoggedInStatement import XApiLoggedInStatement
-from model.models.DataClient import DataClient
 from model.models.DataSubject import DataSubject
-from model.models.Question import Question
+from model.models.Party import Party
+from model.models.QuestionResponse import QuestionResponse
 
 __author__ = "Noah Hummel"
 
 
 @SIG_LOGGED_IN.connect
-def publish_logged_in_xapi_statement(sender: DataClient):
+def publish_logged_in_xapi_statement(sender: Party):
     statement = XApiLoggedInStatement(sender)
     XApiPublisher.get_instance().enqueue(statement)
 
 
 @SIG_ANSWER_SUBMITTED.connect
 def publish_answer_submitted_xapi_statement(
-        sender: DataSubject,
-        question: Question,
-        value: int
+        sender: QuestionResponse
 ):
-    previous_results = filter(lambda r: r.verified, question.get_results_by_subject(sender))
+    question = sender.question
+    subject = next(filter(lambda s: isinstance(s, DataSubject), sender.owners))
+    previous_results = filter(lambda r: r.verified, question.get_results_by_subject(subject))
     receiver = question.dimension.questionnaire.xapi_target
+
     for previous_result in previous_results:
         previous_statement = previous_result.xapi_statement
         if previous_statement is not None and 'id' in previous_statement:
             XApiPublisher.get_instance().void(previous_statement['id'], receiver)
 
-    statement = XApiAnswerSubmittedStatement(sender, question, value)
+    statement = XApiAnswerSubmittedStatement(subject, question, sender.value)
     XApiPublisher.get_instance().enqueue(statement, receiver)
