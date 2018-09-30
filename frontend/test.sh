cb624edd8bd452db3133f5f1182320c5f73630ff
Hannes Leutloff <hannes.leutloff@aol.de>
2018-09-30 17:21:44 +0200

    [FEATURE] move npm/greenkeeper related stuff into frontend test script

--- frontend/test.sh
+++ frontend/test.sh
@@ -2,4 +2,16 @@
 
 image=$1
 
+# cd to frontend dir
+cd "$(dirname "$0")"
+
+npm install
+
+node ./node_modules/.bin/greenkeeper-lockfile-update
+
 echo "No tests configured for frontend."
+
+node ./node_modules/.bin/greenkeeper-lockfile-upload
+
+# cd back
+cd -
