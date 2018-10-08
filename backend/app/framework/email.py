d4ea7f922929dda010bbf8c48dc5b0dd13ac3f57
Noah Hummel <n.p.hummel@gmail.com>
2018-10-08 15:07:56 +0200

    Revert "backend: add sendmail"
    This reverts commit 7d2aed4450aa55e1ecefb4aef8e3649712b070dd.

--- backend/app/framework/email.py
+++ backend/app/framework/email.py
@@ -1,5 +1,3 @@
-from subprocess import Popen, PIPE
-
 import re
 import smtplib
 from email.mime.text import MIMEText
@@ -7,69 +5,34 @@ from typing import List, Pattern
 
 from flask import g
 
-from framework.internationalization import _
-
 __author__ = "Noah Hummel, Hannes Leutloff"
 
 
-def construct_verification_email(questionnaire, verification_token) -> str:
-    if __name__ == '__main__':
-        message = _("Hi there!\n You've recently participated in the survey ") + questionnaire.name + ".\n"
-        message += _("To verify that you're actually you and make your answer count, please follow the link below:\n\n")
-        message += "http://{}/api/response/verify/{}\n\n".format(g._config['DOMAIN_NAME'], verification_token)  # TODO https
-        return message
-
-
-def send_mail(to: str, subject: str, message: str):
-    if g._config['USE_SMTP']:
-        send_mail_smtp(to, subject, message)
-    else:
-        send_mail_sendmail(to, subject, message)
-
-
-def construct_email(sender, recipient, subject, message) -> str:
-    """
-    Constructs RFC2822 compliant email string.
-    :param sender: The sender
-    :param recipient: The recipient
-    :param subject: The subject line
-    :param message: The message body
-    :return: RFC2822 compliant email string
-    """
-    email = MIMEText(message)
-    email['Subject'] = subject
-    email['From'] = sender
-    email['To'] = recipient
-    return email.as_string()
-
-
-def send_mail_smtp(to: str, subject: str, message: str):
+def send_mail(to: str, subject: str, message: str) -> None:
     """
     A helper method to wrap interaction with smtplib in order to send emails.
-
+    
     :param to: str The email address of the recipient.
-    :param subject: str The subject line
+    :param subject: str The subject line 
     :param message: str The mail body
     :return: None
     """
+
     sender = g._config["SMTP_FROM_ADDRESS"]
+    receivers = [to]
     server = g._config["SMTP_SERVER"]
     port = g._config["SMTP_PORT"]
 
-    email =  construct_email(sender, to, subject, message)
+    email = MIMEText(message)
+    email['Subject'] = subject
+    email['From'] = sender
+    email['To'] = to
 
     smtp = smtplib.SMTP(server, port=port)
     if g._config["SMTP_USE_STARTTLS"]:
         smtp.starttls()
     smtp.login(g._config["SMTP_FROM_ADDRESS"], g._config["SMTP_PASSWORD"])
-    smtp.sendmail(sender, [to], email)
-
-
-def send_mail_sendmail(to: str, subject: str, message: str):
-    from_address = "surveytool@{}".format(g._config["DOMAIN_NAME"])
-    email = construct_email(from_address, to, subject, message)
-    p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE, universal_newlines=True)
-    p.communicate(email)
+    smtp.sendmail(sender, receivers, email.as_string())
 
 
 def parse_email_list(emails: List[str]) -> List[Pattern]:
