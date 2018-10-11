ea77cd55ee6862c37312a5509c2292d14d3a0e15
Hannes Leutloff <hannes.leutloff@aol.de>
2018-10-11 18:06:57 +0200

    [TASK] add tets for extractJsonPlusLanguage

--- frontend/src/api/Util/__tests__/Response.js
+++ frontend/src/api/Util/__tests__/Response.js
@@ -1,24 +1,49 @@
-import { extractJson } from "../Response";
+import { extractJson, extractJsonPlusLanguage } from "../Response";
+
+const testContent = {
+  lorem: "ipsum"
+};
+const mockResponse = {
+  json() {
+    return new Promise((resolve, ignore) => {
+      resolve(testContent);
+    });
+  },
+  headers: {
+    get(what) {
+      if (what === "Content-Language") {
+        return "testLanguage";
+      }
+      return "somethingElse";
+    }
+  }
+};
 
 describe("extractJson", () => {
   test("return future of json content", done => {
-    const testContent = {
-      lorem: "ipsum"
-    };
-    const mockJson = {
-      json() {
-        return new Promise((resolve, ignore) => {
-          resolve(testContent);
-        });
-      }
-    };
-
-    extractJson(mockJson).fork(
+    extractJson(mockResponse).fork(
       () => {
         done.fail("Future rejected when it shouldn't.");
       },
       success => {
         expect(success).toEqual(testContent);
+        done();
+      }
+    );
+  });
+});
+
+describe("extractJsonPlusLanguage", () => {
+  test("returns future of content and language", done => {
+    extractJsonPlusLanguage(mockResponse).fork(
+      () => {
+        done.fail("Future rejected when it shouldn't.");
+      },
+      success => {
+        expect(success).toEqual({
+          data: testContent,
+          language: "testLanguage"
+        });
         done();
       }
     );
