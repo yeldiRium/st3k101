91b60ff3186286c982dfeea68d9d6c4b8aea652f
Alexander Kampf <akampftcg@gmail.com>
2019-07-08 17:29:02 +0200

    [FIX] Change wrong name in schema
    Rename data_client to data_subject in QuestionResponseSchema

--- backend/app/api/schema/Response.py
+++ backend/app/api/schema/Response.py
@@ -12,12 +12,12 @@ __author__ = "Noah Hummel"
 class QuestionResponseSchema(RESTFulSchema):
     value = fields.Integer()
     question = fields.Nested(QuestionSchema)
-    data_client = fields.Method('anonymize_data_client')
+    data_subject = fields.Method('anonymize_data_client')
     verified = fields.Boolean()
 
     def anonymize_data_client(self, obj):
-        data_client = next(filter(lambda ow: isinstance(ow, DataSubject), obj.owners))
-        email = data_client.email if data_client else ""
+        data_subject = next(filter(lambda ow: isinstance(ow, DataSubject), obj.owners))
+        email = data_subject.email if data_subject else ""
         hasher = hashlib.sha256()
         hasher.update(email.encode('utf-8'))
         return hasher.digest().hex()
